3723f6b773b1f6d3a3e14ef1924095495d22de5a
Lisa Campbell <lisa@Lisas-MBP.lan>
2019-10-03 11:31:47 -0700

    added shout.

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const server = express();
+server.use(express.json());
+const shouts = [];
 
 server.get('/', (req, res) => {
     res.status(200).json({hello: "Node 22"})
 });
+
+server.get('/shouts', (req, res) => {
+    res.status(200).json(shouts)
+});
+
+server.post('/shouts', (req, res) => {
+    shouts.push(req.body)
+    res.status(201).json(shouts)
+})
 
 //for heroku needs dynamic port
 //we can read port value added by heroku to the environment
